fe83846fc6ab540c71d1efb934339e28ac882a9c
robinlutwick5 <robinlutwick5@gmail.com>
2021-01-23 18:10:33 +0000

    first ever commit

---
+++ 1_basicProgram.py
@@ -0,0 +1,11 @@
+def program_1():
+    a = 2
+    f = 2e4
+    
+    print(a)
+    print("a is an integer")
+    print(f)
+    print("f is a floating point number")
+    
+if __name__=='main__':
+    program_1()
